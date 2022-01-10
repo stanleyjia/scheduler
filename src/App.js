eac615b261c2fa5965e8c37b7e7775c5754f55fc
Stanley Jia <stanleyjia101@gmail.com>
2022-01-10 13:45:28 -0600

    added fetching data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
+const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 const schedule = {
   "title": "CS Courses for 2018-2019",
   "courses": {
@@ -27,12 +28,30 @@ const schedule = {
   }
 };
 
-const App = () =>  (
-  <div className="container">
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </div>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
+  if (!schedule) return <h1>Loading schedule...</h1>;
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </div>
+  );
+};
+
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
